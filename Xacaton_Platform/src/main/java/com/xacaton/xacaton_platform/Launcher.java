84e74601e9b8d2d0223ab2eff898ebb2bdfee717
ShEVa15 <137917375+ShEVa15@users.noreply.github.com>
2025-11-20 11:49:45 +0300

    Delete Xacaton_Platform/src/main/java/com/xacaton/xacaton_platform directory

--- Xacaton_Platform/src/main/java/com/xacaton/xacaton_platform/Launcher.java
+++
@@ -1,7 +0,0 @@
-package com.xacaton.xacaton_platform;
-
-public class Launcher {
-    public static void main(String[] args) {
-        javafx.application.Application.launch(Application.class, args);
-    }
-}
